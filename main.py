72ba0be73390c3c4182264f05a9ec376579e52da
Starkk <42775517+prabal-007@users.noreply.github.com>
2020-07-28 22:54:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from tkinter import *
 import pyshorteners
+import pyperclip
 
 root = Tk()
 root.title('URL shortner')
 root.geometry('480x200')
 root.configure(background='orange')
 def shot():
+    def copy():
+        pyperclip.copy(f'{var2.get()}')
+        pyperclip.paste()
     link = var1.get()
     shortIt = pyshorteners.Shortener()
     x = shortIt.tinyurl.short(link)
     var2 = StringVar()
     var2.set(x)
-    Entry(root, textvariable=var2, font='arial 15 bold',width='30', relief=FLAT).pack(pady=10)
+    frm2 = Frame(root)
+    Entry(frm2, textvariable=var2, font='arial 15 bold',width='30', relief=FLAT).pack(side=LEFT,padx=2)
+    Button(frm2, text='copy',font='arial',command=copy).pack(side=RIGHT,padx=5)
+    frm2.pack(pady=10)
 
 frm1 = Frame(root)
 Label(frm1, text='Enter Link ',font='arial 15 bold', relief=FLAT).pack(side=LEFT)
